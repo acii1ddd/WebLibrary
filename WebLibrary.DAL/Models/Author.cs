ad649561024b7e22d5dfd98f13577c7af70b3b4b
acii1ddd <vitiazagortsev@gmail.com>
2025-10-15 12:49:02 +0300

    start project structure add: added models

---
+++ WebLibrary.DAL/Models/Author.cs
@@ -0,0 +1,13 @@
+namespace WebLibrary.DAL.Models;
+
+public class Author
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    
+    public DateTime DateOfBirth { get; set; }
+
+    public ICollection<Book> Books = [];
+}
+
---
+++ WebLibrary.DAL/Models/Book.cs
@@ -0,0 +1,14 @@
+namespace WebLibrary.DAL.Models;
+
+public class Book
+{
+    public Guid Id { get; set; }
+    
+    public string Title { get; set; } = string.Empty;
+    
+    public int PublishedYear { get; set; }
+    
+    public Author Author { get; set; } = null!;
+    
+    public Guid AuthorId { get; set; }
+}
